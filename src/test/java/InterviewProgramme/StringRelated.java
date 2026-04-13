1b3dc63242ab0488e1fb34dc948d4ca59c6df370
testerakshay <testerakshaysawade@gmail.com>
2026-04-13 10:30:54 +0530

    test1

---
+++ src/test/java/InterviewProgramme/StringRelated.java
@@ -0,0 +1,15 @@
+package InterviewProgramme;
+
+public class StringRelated {
+
+	public static void main(String[] args) {
+		
+		String a= "1234";
+		
+		StringBuffer sb = new StringBuffer(a);
+		//sb.reverse();
+		System.out.println(sb.reverse());
+
+	}
+
+}
